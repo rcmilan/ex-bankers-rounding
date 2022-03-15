99a47a4095fc94af8dc74c4b8bc8ba104098f6ef
Ricardo Milan <ricardo@intercambiodireto.com>
2022-03-15 09:58:18 -0300

    seletor de currency

--- src/pages/money/index.js
+++ src/pages/money/index.js
@@ -1,42 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import MoneyInput from "../../components/moneyInput";
 import MoneyOutput from "../../components/moneyOutput";
 import { setExchangeRate } from "../../features/money/moneySlice";
 import { useSelector, useDispatch } from "react-redux";
 
 function Money() {
+  const [currency, setCurrency] = useState("BRL");
+
   const amount = useSelector((state) => state.money.value);
   const dispatch = useDispatch();
 
   const exchangeRate = 5.0962;
 
+  const currencySelector = () => {
+    switch (currency) {
+      case "BRL":
+        return (
+          <>
+            <button onClick={() => setCurrency("USD")}>usar USD</button>
+            <MoneyInput
+              text={"BRL"}
+              update={(inputValue) => {
+                dispatch(
+                  setExchangeRate({
+                    money: inputValue * exchangeRate,
+                    symbol: "USD",
+                  })
+                );
+              }}
+            />
+          </>
+        );
+      case "USD":
+        return (
+          <>
+            <button onClick={() => setCurrency("BRL")}>usar BRL</button>
+            <MoneyInput
+              text={"USD"}
+              update={(inputValue) => {
+                dispatch(
+                  setExchangeRate({
+                    money: inputValue / exchangeRate,
+                    symbol: "BRL",
+                  })
+                );
+              }}
+            />
+          </>
+        );
+      default:
+        return <>Nenhuma Selecionada</>;
+    }
+  };
+
   return (
     <>
-      <div>
-        <MoneyInput
-          text={"BRL"}
-          update={(inputValue) => {
-            dispatch(
-              setExchangeRate({
-                money: inputValue * exchangeRate,
-                symbol: "USD",
-              })
-            );
-          }}
-        />
-
-        <MoneyInput
-          text={"USD"}
-          update={(inputValue) => {
-            dispatch(
-              setExchangeRate({
-                money: inputValue / exchangeRate,
-                symbol: "BRL",
-              })
-            );
-          }}
-        />
-      </div>
+      <div>{currencySelector()}</div>
       <div>
         <MoneyOutput output={amount} />
       </div>
